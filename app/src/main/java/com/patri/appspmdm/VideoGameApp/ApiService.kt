00bd20442afd0b25d66ff49754a7515722038c06
EowynRohirrim <145437880+EowynRohirrim@users.noreply.github.com>
2024-01-15 19:27:39 +0100

    15/01/2024 Trabajo Tema 6 APIs

--- app/src/main/java/com/patri/appspmdm/VideoGameApp/ApiService.kt
+++ app/src/main/java/com/patri/appspmdm/VideoGameApp/ApiService.kt
@@ -7,10 +7,9 @@ import retrofit2.http.Query
 interface ApiService { //Interfaz
 
     //Co-Rutinas, deja hacer llamadas asincronas
-    @GET("/games?key=6623ace5689e4af8b5376f9f817ef839&page_size=20&search=")
+    @GET("/games?key=6623ace5689e4af8b5376f9f817ef839&page_size=10&search=")
     //empiezan por suspend
     suspend fun getVideoGames(@Query("search") videogameName: String): Response<VideoGameDataResponse>
-
 
     /** la url global es hasta los asteriscos
      * https://api.rawg.io/api/*****games?key=   &page_size=20& search="query
